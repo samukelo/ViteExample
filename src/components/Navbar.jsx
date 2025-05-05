75933e03e0b5c592152eb4bb96a9314755c6ec0f
Samukelo Makofane <samukelomakofane@gmail.com>
2025-05-05 19:40:13 +0200

    new

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,9 +33,9 @@ function CustomNavbar() {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="dashboard">Dashboard</Nav.Link>
-              <Nav.Link href="about">About</Nav.Link>
-              <Nav.Link href="contact">Contact</Nav.Link>
+              <Nav.Link href="dashboard.jsx">Dashboard</Nav.Link>
+              <Nav.Link href="aboutjsx">About</Nav.Link>
+              <Nav.Link href="contactjsx">Contact</Nav.Link>
             </Nav>
             
             {/* Account Section */}
